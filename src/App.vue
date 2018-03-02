131570145d647087a17b8dd6738ffe32ec3e9832
Nidonocu <nidonocu@gmail.com>
2018-03-02 00:58:54 +0000

    Made notes, preped main window

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,33 @@
 <template>
-  <h2>Hello from {{text}}</h2>
+    <h2>Hello from {{text}}</h2>
+    <!-- 
+Counter App:
+* Combat Turn
+* Player:
+ Weapon Magazine Status
+* NPC:
+ HP
+ Armour HP
+ Weapon HP
+ Shield
+* NPC Ship:
+ HP
+ Shield
+ Pilot HP
+* Action Turn Counter
+* Chase Counter
+
+
+    -->
 </template>
 
 <script>
 export default {
-  name: 'test',
-  data () {
-    return {
-       text: ''
+    name: 'App',
+    data () {
+        return {
+            text: ''
+        }
     }
-  }
 }
 </script>
--- src/index.js
+++ src/index.js
@@ -11,28 +11,33 @@ const isDevMode = process.execPath.match(/[\\/]electron/);
 if (isDevMode) enableLiveReload();
 
 const createWindow = async () => {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1600,
-    height: 900,
-  });
+    // Create the browser window.
+        mainWindow = new BrowserWindow({
+        width: 1600,
+        height: 900,
+        show: false
+    });
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/index.html`);
+    // and load the index.html of the app.
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-  // Open the DevTools.
-  if (isDevMode) {
-    await installExtension(VUEJS_DEVTOOLS);
-    mainWindow.webContents.openDevTools();
-  }
+    // Open the DevTools.
+    if (isDevMode) {
+        await installExtension(VUEJS_DEVTOOLS);
+        mainWindow.webContents.openDevTools();
+    }
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
+
+    mainWindow.on('ready', () => {
+        mainWindow.show();
+    })
 };
 
 // This method will be called when Electron has finished
@@ -42,19 +47,19 @@ app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 
 // In this file you can include the rest of your app's specific main process
